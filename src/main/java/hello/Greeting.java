57e7f48a67ec9d635fd93f8ab6fdc86574f06169
samov004 <valery>
2014-12-26 12:24:34 -0500

    Initial commit

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,30 @@
+/**
+ * @file JSONEncodeDemo.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 11/19/2014
+ *
+ * Greeting representation class with resources.
+ */
+
+package hello;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,17 @@
+/**
+ * @file JSONEncodeDemo.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 11/19/2014
+ *
+ * Greeting representation class with resources.
+ */
+
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+}
